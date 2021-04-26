9004751d8c66d987325b31e8f92bdaec5a2a9708
Rucha Yagnik <rpy4499@gmail.com>
2021-04-27 02:14:33 +0530

    Hide Camera button if device type is desktop

--- src/components/Popup/UploadPopup.js
+++ src/components/Popup/UploadPopup.js
@@ -2,19 +2,38 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import CTA from "../Homepage/CTA";
 
-const UploadPopup = ({onClose, onCapture}) => {
+const UploadPopup = ({ onClose, onCapture }) => {
 
   const browseRef = React.createRef();
   const cameraRef = React.createRef();
+
+  const getDeviceType = () => {
+    const userAgent = navigator.userAgent;
+
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(userAgent)) {
+      return "tablet";
+    }
+    if (/Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(userAgent)) {
+      return "mobile";
+    }
+    return "desktop";
+  }
+
+  const deviceType = getDeviceType();
 
   return (
     <>
       <div className="popup">
         <FontAwesomeIcon icon="images" />
-        <CTA icon="folder-open" text="Browse" onClick={() => {browseRef.current.click()}}/>
-        <CTA icon="camera" text="Camera" onClick={() => {cameraRef.current.click()}}/>
-        <input ref={browseRef} type="file" style={{ display: 'none' }} accept="image/*" onChange={(event) => {onCapture(event.target.files[0])}}/>
-        <input ref={cameraRef} type="file" style={{ display: 'none' }} accept="image/*" capture="environment" onChange={(event) => {onCapture(event.target.files[0])}}/>
+        <CTA icon="folder-open" text="Browse" onClick={() => { browseRef.current.click() }} />
+        {
+          deviceType !== "desktop" ?
+            <CTA icon="camera" text="Camera" onClick={() => { cameraRef.current.click() }} />
+            :
+            null
+        }
+        <input ref={browseRef} type="file" style={{ display: 'none' }} accept="image/*" onChange={(event) => { onCapture(event.target.files[0]) }} />
+        <input ref={cameraRef} type="file" style={{ display: 'none' }} accept="image/*" capture="environment" onChange={(event) => { onCapture(event.target.files[0]) }} />
       </div>
       <FontAwesomeIcon className="close-button" icon="window-close" onClick={onClose} />
     </>
